6edb475cf74db46344d7ba54d8c393d3e8f3127f
Charles Joma <almprincecharles@yahoo.com>
2025-03-14 13:21:55 +1100

    Backend :: update document styling

--- backend/styles.js
+++ backend/styles.js
@@ -2,11 +2,11 @@ module.exports = {
   FONT: "Century Gothic",
 
   COLORS: {
-    title: "FF0000", // Red for ceremony title
-    sectionHeader: "0000FF", // Blue for section headers
-    legalText: "008000", // Green for legal text
-    vows: "FF4500", // Orange-Red for vows
-    standardText: "000000", // Black for normal text
+    title: "#FF0000", // Red for ceremony title
+    sectionHeader: "#0000FF", // Blue for section headers
+    legalText: "#008000", // Green for legal text
+    vows: "#FF4500", // Orange-Red for vows
+    standardText: "#000000", // Black for normal text
   },
 
   SIZES: {
