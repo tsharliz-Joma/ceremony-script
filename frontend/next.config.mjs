c0e184e785774214cee62bf8e64d41843c8250e7
charles joma <charles.joma@chepnetwork.com>
2025-03-17 16:49:18 +1100

    Force static exports

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export", // Ensures Next.js works in Amplify
+    images: {
+      unoptimized: true, // Fixes image-related issues in Amplify
+    },
+  };
+  
+  module.exports = nextConfig;
+  
